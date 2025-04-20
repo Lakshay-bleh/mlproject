50f6c8e76b7801de470a4f76b0b408b58e91ef5f
Lakshay <jlakshay05@gmail.com>
2025-04-20 20:51:27 +0530

    Data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,15 +7,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 @dataclass
-class DataiIngestionConfig:
+class DataIngestionConfig:
     train_data_path: str =os.path.join('artifacts',"train.csv")
     test_data_path: str =os.path.join('artifacts',"test.csv")
     raw_data_path: str =os.path.join('artifacts',"raw.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataiIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
     def inititate_data_ingestion(self):
         logging.info("Entered the data ingestion or component")
@@ -45,4 +46,7 @@ class DataIngestion:
         
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.inititate_data_ingestion()
+    train_data,test_data = obj.inititate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
